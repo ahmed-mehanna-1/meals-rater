de97eee39009c6d017a94798a304b41dea1c511f
ahmed-mehanna <ahmadabobakr1@gmail.com>
2022-05-11 00:18:33 +0200

    Finish Token User

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
 from .models import Meal, Rating
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "required": True
+            }
+        }
 
 
 class MealSerializer(serializers.ModelSerializer):
